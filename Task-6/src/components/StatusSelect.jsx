a5d630900c3e8de6693e6b9e4fa702a600906eb5
Kilari-Padmavathi-Kumari <padmavathi.kilari@fissionlabs.com>
2026-06-05 23:18:52 +0530

    Remove Task-6 folder

--- Task-6/src/components/StatusSelect.jsx
+++
@@ -1,9 +0,0 @@
-export const statuses = ['Todo', 'In Progress', 'Done'];
-
-export default function StatusSelect({ value, onChange }) {
-  return (
-    <select value={value} onChange={event => onChange(event.target.value)}>
-      {statuses.map(status => <option key={status}>{status}</option>)}
-    </select>
-  );
-}
